0676b3d528de4d33ceee7ab4b48c1862e971ec09
James Millard <millard.jn@gmail.com>
2017-01-23 22:10:08 +1000

    Alumina Update, Increase receptive field.
    Increase receptive field of first layer.
    Decrease MSRA initialization coefficients.
    Expose downsampling, training loss on linear RGB output, and folder recursion as optional arguments.
    Convert validation error to be reported as a PSNR value.
    Lay ground work for exposing scaling factor as an argument.
    Moved image suppliers into main library.

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,7 @@ extern crate alumina;
 extern crate clap;
 extern crate image;
 
-//mod suppliers;
 mod network;
-
 
 use std::fs::*;
 use std::path::Path;
@@ -18,22 +16,25 @@ use clap::{Arg, App, SubCommand, AppSettings, ArgMatches};
 use bytevec::{ByteEncodable, ByteDecodable};
 use image::GenericImage;
 
-const IMAGENET_PARAMS: &'static [u8] = include_bytes!("res/imagenet.par");
-const ANIME_PARAMS: &'static [u8] = include_bytes!("res/anime.par");
+const IMAGENET_PARAMS: &'static [u8] = include_bytes!("res/imagenet.rsr");
+const IMAGENETLINEAR_PARAMS: &'static [u8] = include_bytes!("res/imagenetlinear.rsr");
+const ANIME_PARAMS: &'static [u8] = include_bytes!("res/anime.rsr");
 
 use network::*;
-use alumina::opt::supplier::*;
-use alumina::opt::supplier::imagefolder::*;
+use alumina::supplier::*;
+use alumina::supplier::imagefolder::*;
 use alumina::graph::*;
 use alumina::shape::*;
-use alumina::opt::asgd::Asgd2;
+use alumina::opt::cain::Cain;
 use alumina::opt::*;
+
+const FACTOR: usize = 3;
 
 fn main() {
 	let app_m = App::new("Rusty SR")
-	.version("v0.0.1")
+	.version("v0.1.0")
 	.author("J. Millard <millard.jn@gmail.com>")
-	.about("A convolutional neural network trained to upscale sRGB images")
+	.about("A convolutional neural network trained to upscale images")
 	.settings(&[AppSettings::SubcommandsNegateReqs, AppSettings::VersionlessSubcommands])
 	.arg(Arg::with_name("INPUT_FILE")
 		.help("Sets the input image to upscale")
@@ -50,7 +51,7 @@ fn main() {
 		.short("p")
 		.long("parameters")
 		.value_name("PARAMETERS")
-		.possible_values(&["imagenet", "anime", "bilinear"])
+		.possible_values(&["imagenet", "imagenetlinear", "anime", "bilinear"])
 		.takes_value(true)
 	)
 	.arg(Arg::with_name("custom")
@@ -58,25 +59,44 @@ fn main() {
 		.short("c")
 		.long("custom")
 		.value_name("PARAMETER_FILE")
-		.help("Sets a custom parameter file to use with the neural net")
+		.help("Sets a custom parameter file (.rsr) to use with the neural net")
 		.takes_value(true)
+	)
+	.arg(Arg::with_name("downsample")
+		.conflicts_with_all(&["parameters", "custom"])
+		.short("d")
+		.long("downsample")
+		.help("Perform downscaling rather than upscaling")
+		.takes_value(false)
 	)
 	.subcommand(SubCommand::with_name("train")
 		.about("Train a new set of neural parameters on your own dataset")
 		.arg(Arg::with_name("PARAMETER_FILE")
 			.required(true)
 			.index(1)
-			.help("Learned network parameters will be (over)written to this file")
+			.help("Learned network parameters will be (over)written to this parameter file (.rsr)")
 		)
 		.arg(Arg::with_name("TRAINING_FOLDER")
 			.required(true)
 			.index(2)
 			.help("Images from this folder(or sub-folders) will be used for training")
 		)
+		.arg(Arg::with_name("LINEAR_LOSS")
+			.short("l")
+			.long("linearLoss")
+			.help("Apply MSE loss to a linearised RGB output rather than sRGB values")
+			.takes_value(false)
+		)
+		.arg(Arg::with_name("RECURSE_SUBFOLDERS")
+			.short("r")
+			.long("recurse")
+			.help("Recurse into subfolders of training and validation folders looking for files")
+			.takes_value(false)
+		)
 		.arg(Arg::with_name("START_PARAMETERS")
 			.short("s")
 			.long("start")
-			.help("Start training from from known parameters rather than random initialisation")
+			.help("Start training from known parameters loaded from this .rsr file rather than random initialisation")
 			.takes_value(true)
 		)
 		.arg(Arg::with_name("VALIDATION_FOLDER")
@@ -93,9 +113,7 @@ fn main() {
 			.help("Set upper limit on number of images used for each validation pass")
 			.takes_value(true)
 		) 
-	)
-
-	.get_matches();
+	).get_matches();
 
 
 	if let Some(sub_m) = app_m.subcommand_matches("train") {
@@ -104,60 +122,71 @@ fn main() {
 	}
 
 	
-	{
-		//-- Sort out parameters and graph
-		let (params, mut graph) = if let Some(file_str) = app_m.value_of("custom") {
-			let mut param_file = File::open(Path::new(file_str)).expect("Error opening parameter file");
-			let mut data = Vec::new();
-			param_file.read_to_end(&mut data).expect("Reading parameter file failed");
-			println!("Upscaling using custom neural net parameters");
-			(<Vec<f32>>::decode::<u32>(&data).expect("ByteVec conversion failed"), sr_net(3, false, 0.0))
-		} else {
-			 match app_m.value_of("parameters") {
-				Some("imagenet") | None => {
-					println!("Upscaling using imagenet neural net parameters");
-					(<Vec<f32>>::decode::<u32>(IMAGENET_PARAMS).expect("ByteVec conversion failed"), sr_net(3, false, 0.0))},
-				Some("anime")           => {
-					println!("Upscaling using anime neural net parameters");
-					(<Vec<f32>>::decode::<u32>(ANIME_PARAMS).expect("ByteVec conversion failed"),    sr_net(3, false, 0.0))},
-				Some("bilinear")        => {
-					println!("Upscaling using bilinear interpolation");
-					(Vec::new(), linear_net())},
-				 _ => unimplemented!(),
-			}
-		};
-		
-
-		let input_image = image::open(Path::new(app_m.value_of("INPUT_FILE").expect("No input file given?"))).expect("Error opening image file");
-
-		let mut out_file = File::create(app_m.value_of("OUTPUT_FILE").expect("No output file given?")).expect("Could not save output file");
-
-
-		let mut input = NodeData::new_blank(DataShape::new(CHANNELS, &[input_image.dimensions().0 as usize, input_image.dimensions().1 as usize], 1));
-
-		img_to_data(&mut input.values, &input_image);
-
-		let output = graph.forward(1, vec![input], &params).remove(0);
-		data_to_img(output).save(&mut out_file, image::ImageFormat::PNG).expect("Could not write output file");
-	}
-
-	println!("Done");
+	upscale(&app_m);
 	
 }
 
 
-fn train(matches: &ArgMatches){
-	
-
-
-	let mut g = sr_net(3, true, 1e-5);
-
-	let mut training_set = ImageFolderSupplier::<ShuffleRandom>::new(Path::new(matches.value_of("TRAINING_FOLDER").expect("No training folder?")), Cropping::Random{width:120, height:120});
-
-	let start_params = if let Some(param_str) = matches.value_of("START_PARAMETERS") {
-		let mut param_file = File::open(Path::new(param_str)).expect("Error opening parameter file");
+fn upscale(app_m: &ArgMatches){
+		
+	//-- Sort out parameters and graph
+	let (params, mut graph) = if let Some(file_str) = app_m.value_of("custom") {
+		let mut param_file = File::open(Path::new(file_str)).expect("Error opening parameter file");
 		let mut data = Vec::new();
 		param_file.read_to_end(&mut data).expect("Reading parameter file failed");
+		print!("Upscaling using custom neural net parameters...");
+		(<Vec<f32>>::decode::<u32>(&data).expect("ByteVec conversion failed"), sr_net(FACTOR, None))
+	} else if app_m.is_present("downsample"){
+		print!("Downsampling using average pooling of linear RGB values...");
+		(Vec::new(), downsample_net(FACTOR))
+	} else {
+		match app_m.value_of("parameters") {
+			Some("imagenet") | None => {
+				print!("Upscaling using imagenet neural net parameters...");
+				(<Vec<f32>>::decode::<u32>(IMAGENET_PARAMS).expect("ByteVec conversion failed"), sr_net(FACTOR, None))},
+			Some("imagenetlinear") => {
+				print!("Upscaling using linear loss imagenet neural net parameters...");
+				(<Vec<f32>>::decode::<u32>(IMAGENETLINEAR_PARAMS).expect("ByteVec conversion failed"), sr_net(FACTOR, None))},
+			Some("anime")           => {
+				print!("Upscaling using anime neural net parameters...");
+				(<Vec<f32>>::decode::<u32>(ANIME_PARAMS).expect("ByteVec conversion failed"),    sr_net(FACTOR, None))},
+			Some("bilinear")        => {
+				print!("Upscaling using bilinear interpolation...");
+				(Vec::new(), bilinear_net(FACTOR))},
+				_ => unreachable!(),
+		}
+	};
+	
+	// TODO: ensure factor argument requires custom neural net file argument.
+	assert_eq!(params.len(), graph.num_params(), "Parameters selected do not have the size required by the neural net. Ensure that the same sample factor is used for upscaling and training", );
+
+	let input_image = image::open(Path::new(app_m.value_of("INPUT_FILE").expect("No input file given?"))).expect("Error opening input image file.");
+
+	let mut out_file = File::create(app_m.value_of("OUTPUT_FILE").expect("No output file given?")).expect("Could not save output file.");
+
+	let mut input = NodeData::new_blank(DataShape::new(CHANNELS, &[input_image.dimensions().0 as usize, input_image.dimensions().1 as usize], 1));
+
+	img_to_data(&mut input.values, &input_image);
+
+	let output = graph.forward(1, vec![input], &params).remove(0);
+	data_to_img(output).save(&mut out_file, image::ImageFormat::PNG).expect("Could not write output file");
+	
+	println!(" Done");
+}
+
+fn train(app_m: &ArgMatches){
+	
+	let linear_loss = app_m.is_present("LINEAR_LOSS");
+
+	let mut g = sr_net(FACTOR, Some((1e-5, linear_loss)));
+	let recurse = app_m.is_present("RECURSE_SUBFOLDERS");
+	let training_set = ImageFolderSupplier::<ShuffleRandom>::new(Path::new(app_m.value_of("TRAINING_FOLDER").expect("No training folder?")), recurse, Cropping::Random{width:192, height:192});
+	let mut training_set = Buffer::new(training_set, 128);
+
+	let start_params = if let Some(param_str) = app_m.value_of("START_PARAMETERS") {
+		let mut param_file = File::open(Path::new(param_str)).expect("Error opening start parameter file");
+		let mut data = Vec::new();
+		param_file.read_to_end(&mut data).expect("Reading start parameter file failed");
 		<Vec<f32>>::decode::<u32>(&data).expect("ByteVec conversion failed")
 	} else {
 		g.init_params()
@@ -165,48 +194,62 @@ fn train(matches: &ArgMatches){
 
 
 
-	let mut solver = Asgd2::new(&mut g);
-	
-	let mut parameter_file = File::create(Path::new(matches.value_of("PARAMETER_FILE").expect("No parameter file?"))).expect("Could not make parameter file");
+	let mut solver = Cain::new(&mut g)
+		.num_subbatches(8)
+		.target_err(0.75)
+		.aggression(0.66)
+		.momentum(0.9)
+		.initial_learning_rate(1e-4)
+		.finish();
+		
+	let param_file_path = Path::new(app_m.value_of("PARAMETER_FILE").expect("No parameter file?")).to_path_buf();
 
-	if let Some(val_str) = matches.value_of("VALIDATION_FOLDER"){ // Add occasional test set evaluation as solver callback
-		let mut g2 = sr_net(3, true, 0.0);
-		let mut validation_set = ImageFolderSupplier::<Sequential>::new(Path::new(val_str), Cropping::Random{width:120, height:120});
+	solver.add_step_callback(move |data|{
+		if data.step_count % 100 == 0 || data.step_count == 1{
+			let mut parameter_file = File::create(&param_file_path).expect("Could not make parameter file");
+			let bytes = data.params.encode::<u32>().expect("ByteVec conversion failed");
+			parameter_file.write_all(&bytes).expect("Could not save to parameter file");
+		}
+		CallbackSignal::Continue
+	});
 
-		let n = if let Some(val_max) = matches.value_of("val_max"){
+
+	if let Some(val_str) = app_m.value_of("VALIDATION_FOLDER"){ // Add occasional test set evaluation as solver callback
+		let mut g2 = sr_net(FACTOR, Some((0.0, linear_loss)));
+		let validation_set = ImageFolderSupplier::<Sequential>::new(Path::new(val_str), recurse, Cropping::None);
+		
+		let n = if let Some(val_max) = app_m.value_of("val_max"){
 			cmp::min(validation_set.epoch_size() as usize, val_max.parse::<usize>().expect("-val_max N must be a positive integer"))
 		} else {
 			validation_set.epoch_size() as usize
 		};
+		let mut validation_set = Buffer::new(validation_set, n);
 
-		solver.add_step_callback(move |_err, step, _evaluations, _graph, params|{
+		solver.add_step_callback(move |data|{
 
-			if step % 100 == 0 {
-				print!("Validation errors:\t");
+			if data.step_count % 100 == 0 || data.step_count == 1{
+
+				let mut err_sum = 0.0;
+				let mut pix_sum = 0.0;
 				for _ in 0..n {
-
 					let (input, training_input) = validation_set.next_n(1);
-					// training_input.push(NodeData::new_blank(DataShape::new_flat(1000, 1)));
+					let pixels = input[0].shape.flat_size_single() as f32;
+					let (batch_err, _, _) = g2.backprop(1, input, training_input, data.params);
 
-					let (batch_err, _, _) = g2.backprop(1, input, training_input, params);
-					
-					print!("{}\t", batch_err)
+					pix_sum += pixels;
+					err_sum += batch_err * pixels;
 				}
-				println!("");
+				let psnr = -10.0*(err_sum/pix_sum).log10();
+				println!("Validation PSNR:\t{}", psnr);
 			}
 
-			if step % 100 == 0 || step == 1{
-				let bytes = params.encode::<u32>().expect("ByteVec conversion failed");
-				parameter_file.write_all(&bytes).expect("Could not save to parameter file");
-			}
-
-
-			true
+			CallbackSignal::Continue
 		});
 	}
 
+	solver.add_boxed_step_callback(max_evals(10_000_000));
+
 	println!("Beginning Training");
-	solver.set_max_evals(10_000_000);
 	solver.optimise_from(&mut training_set, start_params);	
 	println!("Done");
 }
--- src/network.rs
+++ src/network.rs
@@ -12,10 +12,11 @@ use alumina::graph::*;
 
 const CHANNELS: usize = 3;
 
-pub fn sr_net(factor: usize, training: bool, regularisation: f32) -> Graph {
+
+pub fn sr_net(factor: usize, training: Option<(f32, bool)>) -> Graph {
 	let mut g = Graph::new();
 
-	let (input, output) = if training {
+	let (input, output) = if training.is_some() {
 		(g.add_node(Node::new_shaped(CHANNELS, 2, "input")),
 		g.add_node(Node::new_shaped(CHANNELS, 2, "output")))
 	} else {
@@ -23,22 +24,19 @@ pub fn sr_net(factor: usize, training: bool, regularisation: f32) -> Graph {
 		g.add_output_node(Node::new_shaped(CHANNELS, 2, "output")))
 	};
 
-	
 	g.add_operation(LinearInterp::new(&input, &output, &[factor, factor], "linterp"));
 
 	let mut ops: Vec<Box<Operation>> = vec![]; // collect all ops so regularisation can be applied
 
 	let f_conv = g.add_node(Node::new_shaped(32, 2, "f_conv"));
 	let f_activ = g.add_node(Node::new_shaped(32, 2, "f_activ"));
-	ops.push(Convolution::new(&input, &f_conv, &[3, 3], Padding::Same, "conv0", Convolution::init_msra(1.0)));
+	ops.push(Convolution::new(&input, &f_conv, &[5, 5], Padding::Same, "conv0", Convolution::init_msra(1.0)));
 	ops.push(Bias::new(&f_conv, ParamSharing::Spatial, "f_bias", init_fill(0.0)));
 	ops.push(BeLU::new(&f_conv, &f_activ, ParamSharing::Spatial, "f_activ", BeLU::init_porque_no_los_dos()));
 	
-
 	let expand = g.add_node(Node::new_shaped(CHANNELS*factor*factor, 2, "expand"));
 	ops.push(Bias::new(&expand, ParamSharing::Spatial, "expand_bias", init_fill(0.0)));
 	ops.push(Expand::new(&expand, &output, &[factor, factor], "expand"));
-
 
 	for _ in 0..1 { // can have multiple independant chains
 		let n = 32;
@@ -48,7 +46,6 @@ pub fn sr_net(factor: usize, training: bool, regularisation: f32) -> Graph {
 		let l2_activ = g.add_node(Node::new_shaped(n, 2, "l2_activ"));
 		let l3_conv = g.add_node(Node::new_shaped(n, 2, "l3_conv"));
 		let l3_activ = g.add_node(Node::new_shaped(n, 2, "l3_activ"));
-
 
 		ops.push(Bias::new(&l1_conv, ParamSharing::Spatial, "l1_bias", init_fill(0.0)));
 		ops.push(Bias::new(&l2_conv, ParamSharing::Spatial, "l2_bias", init_fill(0.0)));
@@ -60,16 +57,16 @@ pub fn sr_net(factor: usize, training: bool, regularisation: f32) -> Graph {
 
 
 		//-- DenseNet-like Convolution Connections on low-res image
-		ops.push(Convolution::new(&f_activ, &l1_conv, &[5, 5], Padding::Same, "conv1", Convolution::init_msra(1.0)));
-		ops.push(Convolution::new(&f_activ, &l2_conv, &[5, 5], Padding::Same, "conv2", Convolution::init_msra(1.0)));
-		ops.push(Convolution::new(&f_activ, &l3_conv, &[5, 5], Padding::Same, "conv3", Convolution::init_msra(1.0)));
+		ops.push(Convolution::new(&f_activ, &l1_conv, &[5, 5], Padding::Same, "conv1", Convolution::init_msra(0.1)));
+		ops.push(Convolution::new(&f_activ, &l2_conv, &[5, 5], Padding::Same, "conv2", Convolution::init_msra(0.1)));
+		ops.push(Convolution::new(&f_activ, &l3_conv, &[5, 5], Padding::Same, "conv3", Convolution::init_msra(0.1)));
 		//ops.push(Convolution::new(&f_activ, &expand, &[3, 3], Padding::Same, "conv4", Convolution::init_msra(0.1))); // seriously slows down training, maybe causes bad conditioning?
 
-		ops.push(Convolution::new(&l1_activ, &l2_conv, &[3, 3], Padding::Same, "conv5", Convolution::init_msra(1.0)));
-		ops.push(Convolution::new(&l1_activ, &l3_conv, &[3, 3], Padding::Same, "conv6", Convolution::init_msra(1.0)));
+		ops.push(Convolution::new(&l1_activ, &l2_conv, &[3, 3], Padding::Same, "conv5", Convolution::init_msra(0.1)));
+		ops.push(Convolution::new(&l1_activ, &l3_conv, &[3, 3], Padding::Same, "conv6", Convolution::init_msra(0.1)));
 		ops.push(Convolution::new(&l1_activ, &expand, &[3, 3], Padding::Same, "conv7", Convolution::init_msra(0.1)));
 
-		ops.push(Convolution::new(&l2_activ, &l3_conv, &[3, 3], Padding::Same, "conv8", Convolution::init_msra(1.0)));
+		ops.push(Convolution::new(&l2_activ, &l3_conv, &[3, 3], Padding::Same, "conv8", Convolution::init_msra(0.1)));
 		ops.push(Convolution::new(&l2_activ, &expand, &[3, 3], Padding::Same, "conv9", Convolution::init_msra(0.1)));
 
 		ops.push(Convolution::new(&l3_activ, &expand, &[3, 3], Padding::Same, "conv10", Convolution::init_msra(0.1)));
@@ -78,14 +75,14 @@ pub fn sr_net(factor: usize, training: bool, regularisation: f32) -> Graph {
 
 	let op_inds = g.add_operations(ops);
 
-	if regularisation != 0.0 {
-		for op_id in &op_inds {
-			if op_id.num_params == 0 {continue};
-			g.add_secondary_operation(L2Regularisation::new(op_id, regularisation, "L2"), op_id);
-		}
-	}
+	if let Some((regularisation, linear_loss)) = training {
+		if regularisation != 0.0 {
+			for op_id in &op_inds {
+				if op_id.num_params == 0 {continue};
+				g.add_secondary_operation(L2Regularisation::new(op_id, regularisation, "L2"), op_id);
+			}
+		}	
 
-	if training {
 		//let _dummy_training_node = g.add_training_input_node(Node::new_flat(1000, "label")); //imagenet only
 		let input_hr = g.add_input_node(Node::new_shaped(CHANNELS, 2, "input_hr"));
 		let input_hr_lin = g.add_node(Node::new_shaped(CHANNELS, 2, "input_hr_lin"));
@@ -93,8 +90,14 @@ pub fn sr_net(factor: usize, training: bool, regularisation: f32) -> Graph {
 		g.add_operation(SrgbToLinear::new(&input_hr, &input_hr_lin,"srgb2lin"));
 		g.add_operation(Pooling::new(&input_hr_lin, &input_pool, &[factor, factor], "input_pooling"));
 		g.add_operation(LinearToSrgb::new(&input_pool, &input, "lin2srgb"));
-
-		g.add_operation(MseLoss::new(&output, &input_hr, 100.0, "loss"));
+		
+		if linear_loss {
+			let output_lin = g.add_node(Node::new_shaped(CHANNELS, 2, "output_lin"));
+			g.add_operation(SrgbToLinear::new(&output, &output_lin,"srgb2lin_output"));
+			g.add_operation(MseLoss::new(&output_lin, &input_hr_lin, 1.0, "loss"));
+		} else {
+			g.add_operation(MseLoss::new(&output, &input_hr, 1.0, "loss"));
+		}
 
 		g.add_operation(ShapeConstraint::new(&input_hr, &output, &[Arc::new(|d| d), Arc::new(|d| d)], "output_shape"));
 
@@ -105,16 +108,31 @@ pub fn sr_net(factor: usize, training: bool, regularisation: f32) -> Graph {
 	g
 }
 
-pub fn linear_net() -> Graph{
+pub fn bilinear_net(factor: usize) -> Graph{
 	let mut g = Graph::new();
 	let inp = g.add_input_node(Node::new_shaped(CHANNELS, 2, "input"));
 	let lin = g.add_node(Node::new_shaped(CHANNELS, 2, "linear"));
 	let upscale = g.add_node(Node::new_shaped(CHANNELS, 2, "upscale"));
 	let out = g.add_output_node(Node::new_shaped(CHANNELS, 2, "output"));
 	g.add_operation(SrgbToLinear::new(&inp, &lin, "lin"));
-	g.add_operation(LinearInterp::new(&lin, &upscale, &[3, 3], "linterp"));
+	g.add_operation(LinearInterp::new(&lin, &upscale, &[factor, factor], "linterp"));
 	g.add_operation(LinearToSrgb::new(&upscale, &out, "srgb"));
-	g.add_operation(ShapeConstraint::new(&inp, &upscale, &[Arc::new(|d| d*3), Arc::new(|d| d*3)], "triple"));
+	g.add_operation(ShapeConstraint::new(&inp, &upscale, &[Arc::new(move |d| d*factor), Arc::new(move |d| d*factor)], "triple"));
+
+	g
+}
+
+pub fn downsample_net(factor: usize) -> Graph{
+	let mut g = Graph::new();
+
+	let input_hr = g.add_input_node(Node::new_shaped(CHANNELS, 2, "input_hr"));
+	let input_hr_lin = g.add_node(Node::new_shaped(CHANNELS, 2, "input_hr_lin"));
+	let input_pool = g.add_node(Node::new_shaped(CHANNELS, 2, "input_pool"));
+	let output = g.add_output_node(Node::new_shaped(CHANNELS, 2, "output"));
+
+	g.add_operation(SrgbToLinear::new(&input_hr, &input_hr_lin,"srgb2lin"));
+	g.add_operation(Pooling::new(&input_hr_lin, &input_pool, &[factor, factor], "input_pooling"));
+	g.add_operation(LinearToSrgb::new(&input_pool, &output, "lin2srgb"));
 
 	g
 }
